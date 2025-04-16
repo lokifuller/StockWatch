d7199279b72beb9205aecd18d458a0d688fdbe5e
lokifuller <sea.krait8@gmail.com>
2025-04-15 20:57:46 -0700

    Partial fix to extentions sometimes requiring re-entry of the stock ticker to appear

--- StockWatch/StockPriceLoader.cs
+++ StockWatch/StockPriceLoader.cs
@@ -14,53 +14,73 @@ namespace StockPricesApp
 {
     public partial class MainWindow : Window
     {
+
+        private bool isLoading = false;
+
         private async Task LoadStockPricesForSymbolAsync(string symbol)
         {
-            var client = new RestClient("https://twelve-data1.p.rapidapi.com/stocks?exchange=NASDAQ&format=json");
-            var request = new RestRequest("time_series", Method.Get);
-            request.AddParameter("symbol", symbol);
-            request.AddParameter("interval", "1day");
-            request.AddParameter("outputsize", "11");
-            request.AddHeader("X-RapidAPI-Key", "02d91c48ccmsha5de74d7d78c89dp197a54jsn3531436e0019");
-            request.AddHeader("X-RapidAPI-Host", "twelve-data1.p.rapidapi.com");
 
-            var response = await client.ExecuteAsync(request);
+            if (isLoading)
+                return;
 
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            isLoading = true;
+            
+            try
             {
-                dynamic jsonData = JsonConvert.DeserializeObject(response.Content);
-                var prices = jsonData?.values;
-                if (prices != null)
+
+
+                var client = new RestClient("https://twelve-data1.p.rapidapi.com/stocks?exchange=NASDAQ&format=json");
+                var request = new RestRequest("time_series", Method.Get);
+                request.AddParameter("symbol", symbol);
+                request.AddParameter("interval", "1day");
+                request.AddParameter("outputsize", "11");
+                request.AddHeader("X-RapidAPI-Key", "02d91c48ccmsha5de74d7d78c89dp197a54jsn3531436e0019");
+                request.AddHeader("X-RapidAPI-Host", "twelve-data1.p.rapidapi.com");
+
+                var response = await client.ExecuteAsync(request);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
                 {
-                    List<GatherStockPrice> stockPrices = JsonConvert.DeserializeObject<List<GatherStockPrice>>(prices.ToString());
-
-                    StockPricesListBox.Items.Clear();
-
-                    if (isRSIEnabled)
+                    dynamic jsonData = JsonConvert.DeserializeObject(response.Content);
+                    var prices = jsonData?.values;
+                    if (prices != null)
                     {
-                        CalculateAndDisplayRSI(symbol, stockPrices);
-                    }
+                        List<GatherStockPrice> stockPrices = JsonConvert.DeserializeObject<List<GatherStockPrice>>(prices.ToString());
 
-                    if (isSMAEnabled)
-                    {
-                        CalculateAndDisplaySMA(symbol, stockPrices);
-                    }
+                        StockPricesListBox.Items.Clear();
 
-                    if (isVolumeEnabled)
-                    {
-                        CalculateAndDisplayVolume(symbol, stockPrices);
-                    }
+                        if (isRSIEnabled)
+                        {
+                            CalculateAndDisplayRSI(symbol, stockPrices);
+                        }
 
-                    if (isVolumeVsAvgEnabled)
-                    {
-                        CalculateAndDisplayVolumeComparison(symbol, stockPrices);
-                    }
+                        if (isSMAEnabled)
+                        {
+                            CalculateAndDisplaySMA(symbol, stockPrices);
+                        }
 
-                    DisplayStockPrices(symbol, stockPrices);
+                        if (isVolumeEnabled)
+                        {
+                            CalculateAndDisplayVolume(symbol, stockPrices);
+                        }
+
+                        if (isVolumeVsAvgEnabled)
+                        {
+                            CalculateAndDisplayVolumeComparison(symbol, stockPrices);
+                        }
+
+                        DisplayStockPrices(symbol, stockPrices);
+                    }
                 }
-            }
 
-            SaveCurrentStockSymbol(symbol);
+                SaveCurrentStockSymbol(symbol);
+            }
+            
+            finally
+
+            {
+                isLoading = false;
+            }
         }
     }
 }
